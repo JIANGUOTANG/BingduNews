d5523fe0669acc3b2ae6f06e3c6be9aa9ddda6a7
JIANGUOTANG <1183328120@qq.com>
2016-10-22 17:01:19 +0800

    new class

---
+++ app/src/main/java/com/example/jian/bingdu/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.jian.bingdu;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jian on 2016/10/22.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+    }
+}
